12c73cec0cbf384da0ca8992aed8de95e582ef15
davidmglenn <davidglenn520@gmail.com>
2020-09-06 15:26:02 -0600

    Update all.js

--- src/all.js
+++ src/all.js
@@ -204,7 +204,7 @@ class AllBot {
     );
 
     // Mention @all command
-    this.robot.hear(/beefbot (.+)/i, res => this.beefResponse(res, res.match[0], res.match[1]));
+    this.robot.hear(/beefbot (.+)/i, res => this.beefResponse(res, res.match[1], res.match[2]));
   }
 }
 
